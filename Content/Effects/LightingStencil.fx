2cb021e6866b54afb4ae31d9d1eb0ebf3a7c3ca2
Andrew <54269534+awu2147@users.noreply.github.com>
2020-09-21 00:09:42 +0100

    Started work on texture importing.

---
+++ Content/Effects/LightingStencil.fx
@@ -0,0 +1,63 @@
+﻿#if OPENGL
+	#define SV_POSITION POSITION
+	#define VS_SHADERMODEL vs_3_0
+	#define PS_SHADERMODEL ps_3_0
+#else
+	#define VS_SHADERMODEL vs_4_0_level_9_1
+	#define PS_SHADERMODEL ps_4_0_level_9_1
+#endif
+
+Texture2D LightingRender;
+sampler2D LightingRenderSampler = sampler_state
+{
+	Texture = <LightingRender>;
+}; 
+
+Texture2D LightingStencilRender;
+sampler2D LightingStencilRenderSampler = sampler_state
+{
+	Texture = <LightingStencilRender>;
+};
+
+float4 CurrentLighting;
+
+struct VertexShaderOutput
+{
+	float4 Position : SV_POSITION;
+	float4 Color : COLOR0;
+	float2 TextureCoordinates : TEXCOORD0;
+};
+
+float4 MainPS(VertexShaderOutput input) : COLOR
+{
+	float4 color = tex2D(LightingRenderSampler, input.TextureCoordinates);
+	float4 colorStencil = tex2D(LightingStencilRenderSampler, input.TextureCoordinates);
+	
+	// Green = Current(Anti)Lighting, Red = Lighting, Black = Deco Stencil (AntiShadow mask).
+	// Propagate green to red and black.
+    color.g = CurrentLighting.g;
+	// Now subtract red channel value from green channel value.
+	// The smaller the result the brighter the light (following subtrative blendstate).
+    float green = color.g;
+    float red = color.r;
+    float diff = green - red;
+	// Assign result to all channels instead of just green.
+    color.rgb = float3(diff, diff, diff);
+	// Use second stencil to trim places where light overlaps with background.
+    if (colorStencil.g == CurrentLighting.g)
+	{ 
+        return color.rgba = CurrentLighting.ggga;
+    }
+	else
+	{
+		return color;
+	}
+}
+
+technique SpriteDrawing
+{
+	pass P0
+	{
+		PixelShader = compile PS_SHADERMODEL MainPS();
+	}
+};
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,19 +1,15 @@
-﻿using SunbirdMB.Gui;
+﻿using SunbirdMB.Core;
+using SunbirdMB.Framework;
+using SunbirdMB.Gui;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.IO;
 
 namespace SunbirdMB
 {
@@ -22,9 +18,12 @@ namespace SunbirdMB
     /// </summary>
     public partial class MainWindow : Window
     {
+        public ObservableCollection<Actor> ActorList { get; set; } = new ObservableCollection<Actor>();
+
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = this;
             Logger.DataContext = new LoggerViewModel();
             MainGameWindow.Loaded += MainGameWindow_Loaded;
             Closed += MainWindow_Closed;
@@ -46,11 +45,44 @@ namespace SunbirdMB
             Debug.Print(MainGamePanel.ActualHeight.ToString());
             MainGameWindow.SetCameraTransformMatrix((int)MainGamePanel.ActualWidth, (int)MainGamePanel.ActualHeight);
             SizeChanged += MainWindow_SizeChanged;
+
+            var appPath = Assembly.GetExecutingAssembly().Location;
+            string contentPath = Path.Combine(appPath, "..", "..", "..","Content");
+            var test = Path.Combine(contentPath, CubeFactory.CubeTopMetaDataLibrary[0].Path + ".png");
+            test.Log();
+
+            ActorList.Add(new Actor(test, new Int32Rect(0, 0, 72, 75)));
+            ActorList.Add(new Actor(test, new Int32Rect(0, 75, 72, 75)));
+            ActorList.Add(new Actor(test, new Int32Rect(72, 0, 72, 75)));
+            ActorList.Add(new Actor(test, new Int32Rect(72, 75, 72, 75)));
         }
 
         private void Clear_Log_Click(object sender, RoutedEventArgs e)
         {
             LoggerViewModel.Log.Clear();
+        }
+
+        private void Image_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            "clicked".Log();
+        }
+    }
+
+    public class Actor
+    {
+        public string Image { get; set; }
+
+        public Int32Rect SourceRect { get; set; }
+
+        public Actor(string imagePath) : this(imagePath, Int32Rect.Empty)
+        {
+            Image = imagePath;
+        }
+
+        public Actor(string imagePath, Int32Rect sourceRect)
+        {
+            Image = imagePath;
+            SourceRect = sourceRect;
         }
     }
 }
