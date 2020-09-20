0d22a389c964b2f0e30a06009ae6963685cd6557
Andrew <54269534+awu2147@users.noreply.github.com>
2020-09-20 14:27:16 +0100

    Initial commit

---
+++ MainGame.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using MonoGame.Framework.WpfInterop;
+using MonoGame.Framework.WpfInterop.Input;
+using System;
+using System.Diagnostics;
+
+namespace SunbirdMB
+{
+    public class MainGame : WpfGame
+    {
+        private IGraphicsDeviceService _graphicsDeviceManager;
+        private SpriteBatch _spriteBatch;
+        private WpfKeyboard _keyboard;
+        private WpfMouse _mouse;
+        public Color renderColor = Color.CornflowerBlue;
+
+        protected override void Initialize()
+        {
+            // must be initialized. required by Content loading and rendering (will add itself to the Services)
+            // note that MonoGame requires this to be initialized in the constructor, while WpfInterop requires it to
+            // be called inside Initialize (before base.Initialize())
+            _graphicsDeviceManager = new WpfGraphicsDeviceService(this);
+
+            // wpf and keyboard need reference to the host control in order to receive input
+            // this means every WpfGame control will have it's own keyboard & mouse manager which will only react if the mouse is in the control
+            _keyboard = new WpfKeyboard(this);
+            _mouse = new WpfMouse(this);
+
+            // must be called after the WpfGraphicsDeviceService instance was created
+            base.Initialize();
+
+            // content loading now possible
+        }
+
+        protected override void Update(GameTime time)
+        {
+            // every update we can now query the keyboard & mouse for our WpfGame
+            var mouseState = _mouse.GetState();
+            if (mouseState.LeftButton == ButtonState.Pressed)
+            {
+                Debug.Print("down");
+            }
+            var keyboardState = _keyboard.GetState();
+        }
+
+        protected override void Draw(GameTime time)
+        {
+            // get and cache the wpf rendertarget (there is always a default rendertarget)
+            var wpfRenderTarget = (RenderTarget2D)GraphicsDevice.GetRenderTargets()[0].RenderTarget;
+            GraphicsDevice.SetRenderTarget(wpfRenderTarget);
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            //_spriteBatch.Begin();
+            //_spriteBatch.Draw(this.rendertarget, Vector2.Zero, Color.White);
+            //_spriteBatch.End();
+        }
+    }
+}
