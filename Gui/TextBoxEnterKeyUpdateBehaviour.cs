3d95e8c5929fe2555b0e746410472c1da4f7fcc3
Andrew <54269534+awu2147@users.noreply.github.com>
2020-09-30 01:21:31 +0100

    We now use wpf.behaviours package.

---
+++ Gui/TextBoxEnterKeyUpdateBehaviour.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using Microsoft.Xaml.Behaviors;
+using Microsoft.Xna.Framework.Input;
+using SunbirdMB.Framework;
+
+namespace SunbirdMB.Gui
+{
+    public class TextBoxEnterKeyUpdateBehavior : Behavior<TextBox>
+    {
+        protected override void OnAttached()
+        {
+            if (this.AssociatedObject != null)
+            {
+                base.OnAttached();
+                this.AssociatedObject.KeyDown += AssociatedObject_KeyDown;
+            }
+        }
+
+        protected override void OnDetaching()
+        {
+            if (this.AssociatedObject != null)
+            {
+                this.AssociatedObject.KeyDown -= AssociatedObject_KeyDown;
+                base.OnDetaching();
+            }
+        }
+
+        private void AssociatedObject_KeyDown(object sender, KeyEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            if (textBox != null)
+            {
+                if (e.Key == Key.Return)
+                {
+                    if (e.Key == Key.Enter)
+                    {
+                        textBox.MoveFocus(new TraversalRequest(FocusNavigationDirection.Next));
+                        System.Windows.Input.Keyboard.ClearFocus();
+                    }
+                }
+            }
+        }
+    }
+}
